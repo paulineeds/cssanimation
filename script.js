47307d778a5a3dcea2314b72a8c99310fcfa40bf
Pauline De Smet <paulinedesmet@me.com>
2019-06-05 16:26:25 +0200

    fix smooth scroll

---
+++ script.js
@@ -0,0 +1,13 @@
+
+    const target = document.getElementById('refer');
+    target.addEventListener("click", clickTarget);
+
+function clickTarget () {
+    event.preventDefault;
+   document.getElementById('two').scrollIntoView(false)
+console.log(event.target);
+}
+
+
+
+
